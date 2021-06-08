b5a6f78ce9d2c3969a7d42b26e20cff1a8a282eb
Btara Truhandarien <10100624+btruhand@users.noreply.github.com>
2021-06-08 01:54:14 -0400

    use etherscan in scheduled lambda to get stETH balance

--- steth-scheduled-tracker/handler.js
+++ steth-scheduled-tracker/handler.js
@@ -1,6 +1,24 @@
 'use strict';
 
+const axios = require('axios').default.create({
+  baseURL: 'https://api.etherscan.io/',
+  params: {
+    address: process.env.ETH_ADDRESS,
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    // this is Lido's stETH contract address: https://etherscan.io/address/0xae7ab96520de3a18e5e111b5eaab095312d7fe84
+    contractaddress: '0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
+  }
+});
+
 module.exports.run = async (event, context) => {
   const time = new Date();
-  console.log(`Your cron function "${context.functionName}" ran at ${time}`);
+  console.log(`"${context.functionName}"  running at ${time} with event ${JSON.stringify(event)} and context ${JSON.stringify(context)}`);
+  const { data } = await axios.get('api', {
+    params: {
+      module: 'account',
+      action: 'tokenbalance',
+      tag: 'latest'
+    }
+  });
+  console.log('data response from EtherScan', data);
 };
