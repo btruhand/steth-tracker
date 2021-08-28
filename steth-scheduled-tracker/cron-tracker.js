d6b811cd86f8b3094acbf418d56c4ac416ea3960
Btara Truhandarien <10100624+btruhand@users.noreply.github.com>
2021-08-28 17:06:09 -0400

    feat: add e-mailing result capability to cron-tracker

--- steth-scheduled-tracker/cron-tracker.js
+++ steth-scheduled-tracker/cron-tracker.js
@@ -2,6 +2,7 @@ const { getCurrentBalance } = require('./steth-tracking');
 const { cleanEnv, str, makeValidator } = require('envalid');
 const fs = require('fs');
 const { serializeError } = require('serialize-error');
+const { execSync } = require('child_process');
 
 const file = makeValidator((filePath) => {
   try {
@@ -17,7 +18,8 @@ function getValidatedEnvironment() {
     ETH_ADDRESS: str({ desc: 'Ethereum address that holds stETH to be tracked' }),
     ETHERSCAN_API_KEY: str({ desc: 'EtherScan API Key' }),
     BALANCE_FILE: file({ desc: 'File path that should contain the balance file' }),
-    REBASING_TRACKING_FILE: file({ desc: 'File path that contains rebasing information' })
+    REBASING_TRACKING_FILE: file({ desc: 'File path that contains rebasing information' }),
+    EMAIL_ADDRESS: str({ desc: 'email address to send results to' })
   });
 }
 
@@ -59,15 +61,47 @@ function updateBalanceFile(balanceFile, newBalance) {
   fs.writeFileSync(balanceFile, `${newBalance}\n`);
 }
 
+/**
+ * @param {{
+ *  total_balance?: String,
+ *  rebase_amount?: String,
+ *  error?: String 
+ * }} results
+ * @param {string} subject 
+ * @param {to} subject
+ */
+function mail(results, subject, to) {
+  const mailContent = `
+    Total balance: ${results.total_balance !== undefined ? results.total_balance : 'N/A'}
+    Rebased amount: ${results.rebase_amount != undefined ? results.rebase_amount : 'N/A'}
+    Date: ${new Date()}
+    Error: ${results.error ? results.error : 'N/A'}
+  `;
+  execSync(`echo "${mailContent}" | mail -s "${subject}" ${to}`);
+}
+
+const env = getValidatedEnvironment();
+
+/**
+ * @returns {Promise<{
+ *  total_balance: String
+ *  rebase_amount: String
+ * }>}
+ */
 async function main() {
-  const env = getValidatedEnvironment();
   const currentBalance = await getCurrentBalance(env.ETH_ADDRESS, env.ETHERSCAN_API_KEY);
   const previousBalance = readBalanceFromFile(env.BALANCE_FILE);
+  /**
+   * @type {BigInt}
+   */
   const rebaseAmount = currentBalance - previousBalance;
   updateBalanceFile(env.BALANCE_FILE, currentBalance);
   addRebaseToFile(rebaseAmount, env.REBASING_TRACKING_FILE);
+  return { total_balance: formatErc20TokenValue(currentBalance), rebase_amount: formatErc20TokenValue(rebaseAmount) }
 }
 
-main().catch(err => {
-  console.log(`An error happened when running cron tracker: ${JSON.stringify(serializeError(err))}`);
-});
+const EMAIL_SUBJECT = 'stETH rebase calculation result';
+
+main()
+  .then(results => mail(results, `[SUCCESS] ${EMAIL_SUBJECT}`, env.EMAIL_ADDRESS))
+  .catch(err => mail({ error: serializeError(err) }, `[FAILURE] ${EMAIL_SUBJECT}`, env.EMAIL_ADDRESS));
